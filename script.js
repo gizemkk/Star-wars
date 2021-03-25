2cbd7472d7b2640ea92518f16029a7fb1c4c8374
Gizem Karadagoglu <karadagoglugizem126@gmail.com>
2021-03-25 16:45:20 -0700

    Star-war project added!

---
+++ script.js
@@ -0,0 +1,186 @@
+
+const output = document.getElementById("output");
+const spinner = document.getElementById("spinner");
+const url = "https://starwars.egghead.training/";
+const btn=document.getElementById('button1')
+
+function starWars(endpoint){
+
+    return fetch(url + endpoint)
+    .then((response)=>{
+        if(response.ok){
+            return response.json()
+        }
+        else{
+           return Promise.reject(Error("unsuccescful response"));
+        }    
+
+    })
+    .then(data=>{
+        // console.log(`There are ${data.length} ${endpoint} in Starwars history`)
+        console.log(data)
+        data.forEach(element => {console.log(`Film episode_${element.episode_id} title is ${element.title}`)})
+     
+
+    })
+       .catch((err)=>{ console.log(err)
+        const output = document.getElementById("output");
+        const spinner = document.getElementById("spinner");
+        const url = "https://starwars.egghead.training/";
+        const btn=document.getElementById('button1')
+        
+        function starWars(endpoint){
+        
+            return fetch(url + endpoint)
+            .then((response)=>{
+                if(response.ok){
+                    return response.json()
+                }
+                else{
+                   return Promise.reject(Error("unsuccescful response"));
+                }    
+        
+            })
+            .then(data=>{
+                // console.log(`There are ${data.length} ${endpoint} in Starwars history`)
+                console.log(data)
+                data.forEach(element => {console.log(`Film episode_${element.episode_id} title is ${element.title}`)})
+             
+        
+            })
+               .catch((err)=>{ console.log(err)
+        
+               }) ;
+        
+               }
+            
+        
+               function starWarsCount(endpoint){
+        
+                return fetch(url + endpoint)
+                .then((response)=>{
+                    if(response.ok){
+                        return response.json()
+                    }
+                    else{
+                       return Promise.reject(Error("unsuccescful response"));
+                    }    
+            
+                })
+            }
+                
+                starWarsCount('planets').then(response =>{
+                    
+                    console.log(response)
+                })
+                starWarsCount('starships').then(response =>{
+                    
+                    console.log(response)
+                })
+                starWarsCount('films').then(response =>{
+        
+                    console.log(response)
+                })
+               
+          Promise.all([starWarsCount('films'),starWarsCount('planets'),starWarsCount('species'),
+          starWarsCount('people'),starWarsCount('starships'),starWarsCount('vehicles')])
+          .then(([films,planets,species,people,starships,vehicles])=>{
+              output.innerHTML =  `${films.length} films ,` + `${planets.length} planets, ` + `${species.length} species ,` +
+              `${people.length} people,` + `${starships.length} starships, ` + `${vehicles.length} vehicles, ` 
+          }) .catch((err) =>{
+              console.log(err)
+              
+          
+          })
+         .finally(()=>{spinner.src='images/star-wars-ship-2.png'})
+         btn.addEventListener('click',()=>{
+             starWarsCount('starships').then(response =>{
+                //  console.log(response)
+                 
+                const randomNumber= Math.floor(Math.random()* response.length)
+                 spinner.src=url + response[randomNumber].image
+             })
+        
+        
+        
+         })
+         
+        
+         
+        
+        
+        
+        
+        //    starWars('films')
+        
+            
+
+       }) ;
+
+       }
+    
+
+       function starWarsCount(endpoint){
+
+        return fetch(url + endpoint)
+        .then((response)=>{
+            if(response.ok){
+                return response.json()
+            }
+            else{
+               return Promise.reject(Error("unsuccescful response"));
+            }    
+    
+        })
+    }
+        
+        starWarsCount('planets').then(response =>{
+            
+            console.log(response)
+        })
+        starWarsCount('starships').then(response =>{
+            
+            console.log(response)
+        })
+        starWarsCount('films').then(response =>{
+
+            console.log(response)
+        })
+       
+  Promise.all([starWarsCount('films'),starWarsCount('planets'),starWarsCount('species'),
+  starWarsCount('people'),starWarsCount('starships'),starWarsCount('vehicles')])
+  .then(([films,planets,species,people,starships,vehicles])=>{
+      output.innerHTML =  `${films.length} films ,` + `${planets.length} planets, ` + `${species.length} species ,` +
+      `${people.length} people,` + `${starships.length} starships, ` + `${vehicles.length} vehicles, ` 
+  }) .catch((err) =>{
+      console.log(err)
+      
+  
+  })
+ .finally(()=>{spinner.src='images/star-wars-ship-2.png'})
+ btn.addEventListener('click',()=>{
+     starWarsCount('starships').then(response =>{
+        //  console.log(response)
+         
+        const randomNumber= Math.floor(Math.random()* response.length)
+         spinner.src=url + response[randomNumber].image
+     })
+
+
+
+ })
+ 
+
+ 
+
+
+
+
+//    starWars('films')
+
+    
+
+
+
+
+
